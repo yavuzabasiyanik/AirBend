b25f455b10610a481b6c758e4bdd226bb4c19db7
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-03-03 15:30:50 -0600

    validationKindADone

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,12 +3,34 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
 
 const router = express.Router();
 
 
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+
+
+
+
+// Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -29,7 +51,7 @@ router.post(
         });
     })
 );
-
+// Log out
 router.delete(
     '/',
     (_req, res) => {
@@ -39,6 +61,18 @@ router.delete(
 );
 
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
 
 
 
