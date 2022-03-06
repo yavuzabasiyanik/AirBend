f82873c124d7e5945516c80275db43390917ed8d
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-03-06 17:02:26 -0600

    imgdone

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -36,8 +36,8 @@ router.post(
     '/',
     validateSignup,
     asyncHandler(async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
+      const { email, password, username,profileUrl } = req.body;
+      const user = await User.signup({ email, username, password,profileUrl });
 
       await setTokenCookie(res, user);
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -39,13 +39,17 @@ export const restoreUser = () => async dispatch => {
 };
 
 export const signup = (user) => async (dispatch) => {
-  const { username, email, password } = user;
+  const { username, email, password,profileImg } = user;
+
+  console.log(profileImg);
+
   const response = await csrfFetch("/api/users", {
     method: "POST",
     body: JSON.stringify({
       username,
       email,
       password,
+      profileUrl:profileImg
     }),
   });
   const data = await response.json();
