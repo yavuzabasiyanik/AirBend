e1d05dc0fbd537350ec8eccbc05bd65785d5c7c3
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-03-07 11:03:31 -0600

    testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-
+const spotsRouter = require('./spots');
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
 
-
+router.use('/spots', spotsRouter)
 
 
 
---
+++ backend/routes/api/spots.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const router = express.Router();
+
+
+router.get('/', asyncHandler(async(req,res)=>{
+
+    
+}))
+
+
+
+
+
+
+
+
+
+module.exports = router;
