3ca6be692d315117099ab6dbed7bc0f3257fbb32
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-03-09 01:14:29 -0600

    forherokupush

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -104,40 +104,6 @@ export const deleteSpotThunk = (payload) => async (dispatch) => {
 
 };
 
-// export const createBooking = (payload) => async (dispatch) => {
-//     const response = await csrfFetch(`/api/spots/booking`, {
-//         method: 'POST',
-//         body: JSON.stringify(payload)
-//     })
-
-//     if (response.ok) {
-
-//         const data = await response.json();
-
-//         dispatch(addBooking(data));
-//     }
-
-//     return response;
-//     // we dont need to return response
-
-// }
-
-
-// export const deleteBooking = ({id,spotId}) => async (dispatch) => {
-//     const response = await csrfFetch(`/api/spots/${spotId}/bookings/${id}`, {
-//         method: 'DELETE'
-//     });
-
-//     // console.log(id,spotId);
-//     if (response.ok) {
-
-//         const {id,spotId} = await response.json();
-
-//         dispatch(deleteBookingAction({id,spotId}));
-
-//     }
-
-// }
 
 const initialState = { spots: {} };
 
@@ -158,37 +124,30 @@ const spotReducer = (state = initialState, action) => {
             return newState;
         case ADD_SPOT:
             newState = { ...state };
-            newState.spots[action.spot.id] = { ...action.spot };
+            newSpots= {...state.spots};
+            newSpots[action.spot.id] = action.spot;
+            newState.spots = {...newSpots};
             return newState;
         case EDIT_SPOT:
             newState = { ...state };
-            newState.spots[action.spot.id] = { ...newState.spots[action.spot.id], ...action.spot };
+            newSpots = {...state.spots};
+            newSpots[action.spot.id]  = {...newSpots[action.spot.id], ...action.spot}
+            // newState.spots[action.spot.id] = { ...newState.spots[action.spot.id], ...action.spot };
+
+            newState.spots = {...newSpots};
+            
             return newState;
         case DELETE_SPOT:
             newState = { ...state };
-            delete newState.spots[action.id];
+            newSpots = {...state.spots};
+
+            delete newSpots[action.id];
+
+
+            newState.spots = { ...newSpots };
+
+
             return newState;
-        // case ADD_BOOKING:
-        //     newState = { ...state };
-        //     newBooking = newState.spots[action.booking.spotId].Bookings;
-        //     newBooking.push(action.booking);
-        //     newState.spots[action.booking.spotId] = { ...newState.spots[action.booking.spotId], newBooking };
-        //     return newState;
-        // case DELETE_BOOKING:
-        //     newState = { ...state };
-        //     newBooking = newState.spots[action.spotId].Bookings;
-
-
-        //     newBooking.forEach((e)=>{
-
-        //         if(e.id=== +action.id){
-        //             newBooking.splice(newBooking.indexOf(e),1);
-        //         }
-        //     })
-
-        //     newState.spots[action.spotId] = { ...newState.spots[action.spotId], newBooking };
-
-        //     return newState;
         default:
             return state;
     }
